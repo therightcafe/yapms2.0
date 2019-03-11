b849ab1233250a7682cb4df8364618ba4505f29e
Jacob Enders <jacob@Jacob-Laptop>
2019-03-11 17:16:50 -0400

    updated admin panel to add source

--- news/admin/send.php
+++ news/admin/send.php
@@ -19,6 +19,8 @@
 	$text= trim($_POST["text"]);
 	//$text = filter_var($text, FILTER_SANITIZE_SPECIAL_CHARS);
 
+	$source = trim($_POST["source"]);
+
 	$featured = trim($_POST["featured"]);
 	$f;
 	if($featured) {
@@ -34,7 +36,7 @@
 		$featured . ")";
 */
 
-	$sql = 'insert into articles (title, author, date, snippet, text, Featured) values (?,?,?,?,?,?)';
+	$sql = 'insert into articles (title, author, date, snippet, text, source, Featured) values (?,?,?,?,?,?,?)';
 	$stm = $dbh->prepare($sql);
 	
 	if($stm->execute([$title, $author, date("Y-m-d H:i:s"), $snippet, $text, $f])) {
