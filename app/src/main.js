58c182f3c459680235e0fa47deef0857e988878c
Jacob Enders <jacobenders1@gmail.com>
2019-05-12 22:11:23 -0400

    updated version

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,4 @@
-var currentCache = 'v0.11.40';
+var currentCache = 'v0.11.41';
 
 var states = [];
 var lands = [];
--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-var currentCache = 'v0.11.40';
+var currentCache = 'v0.11.41';
 
 function swLog(message) {
 	console.log('SW' + currentCache + ': ' + message);
