47067c59ecdc15dd0c3369ab30a644a3c798a3e4
Jacob Enders <jacobenders1@gmail.com>
2019-05-22 14:11:47 -0400

    update sw

--- app/sw.js
+++ app/sw.js
@@ -1,10 +1,10 @@
-var currentCache = 'v0.15.7';
+var currentCache = 'v0.15.8';
 
 function swLog(message) {
 	console.log('SW' + currentCache + ': ' + message);
 }
 
-//self.skipWaiting();
+self.skipWaiting();
 
 self.addEventListener('install', function(event) {
 	event.waitUntil(
