a0e21c832f632b988c224d17e7c29275c0f42547
Jacob Enders <jacobenders1@gmail.com>
2019-05-30 11:43:48 -0400

    added ads to home page

--- index.php
+++ index.php
@@ -177,6 +177,18 @@
 	</div>
 	</div>
 
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- homepage -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1660456925957249"
+     data-ad-slot="9877032120"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
 	<div class="map-type-box">
 	<div class="map-type-header">
 		USA - Historical Maps
