56e479b0dc9a5315a1a593d5d8d979b02caf86d9
Jacob Enders <jacobenders1@gmail.com>
2019-06-28 15:43:42 -0400

    reuse code for other rss feeds'

--- news/admin/auto.php
+++ news/admin/auto.php
@@ -3,57 +3,52 @@ include 'dblogin.php';
 $sql = 'select title from articles';
 $titles = $dbh->query($sql);
 
-foreach($titles as $row) {
-	echo $row['title'] . '<br>';
-}
+function autoSubmit($preTitle, $url) {
+	$content = file_get_contents($url);
+	$xml = new SimpleXMLElement($content);
 
-$content = file_get_contents('https://www.congress.gov/rss/presented-to-president.xml');
+	echo '<ul>';
+	foreach($xml->channel->item as $entry) {
+		echo '<li><a href=' . $entry->link . ' title=' . $entry->title . '>'. $entry->title . '</a></li>' . $entry->description;
 
-echo $content;
+		$title = $preTitle . $entry->title;
+		$author = 'Congress';
+		$snippet = $entry->description;
+		$text = $entry->description;
+		$published = strtotime($xml->channel->pubDate);
+		$published = date('Y-m-d', $published);
+		$source = $entry->link;
+		$featured = false;
 
-$xml = new SimpleXMLElement($content);
-
-echo '<ul>';
-
-foreach($xml->channel->item as $entry) {
-	echo '<li><a href=' . $entry->link . ' title=' . $entry->title . '>'. $entry->title . '</a></li>' . $entry->description;
-
-	$title = 'Presidential Signature: ' . $entry->title;
-	$author = 'Congress';
-	$snippet = $entry->description;
-	$text = $entry->description . '<br> <iframe src="' . $entry->link . '"></iframe>';
-	$published = strtotime($xml->channel->pubDate);
-	$published = date('Y-m-d', $published);
-	$source = $entry->link;
-	$featured = false;
-
-	$skipUpload = false;
-	foreach($titles as $row) {
-		if($title === $row['title']) {
-			$skipUpload = true;
-			break;	
+		$skipUpload = false;
+		foreach($titles as $row) {
+			if($title === $row['title']) {
+				$skipUpload = true;
+				break;	
+			}
 		}
-	}
 
-	if($skipUpload === false) {
+		if($skipUpload === false) {
+			$sql = 'insert into articles (title, author, published, upload, snippet, text, source, Featured) values (?,?,?,?,?,?,?,?)';
+			echo '<br>';
+			$stm = $dbh->prepare($sql);
 
-		$sql = 'insert into articles (title, author, published, upload, snippet, text, source, Featured) values (?,?,?,?,?,?,?,?)';
+			if($stm->execute([$title, $author, $published, date("Y-m-d H:i:s"), $snippet, $text, $source, $featured])) {
+				echo 'sql query success...<br>';
+			} else {
+				echo 'sql query failed...<br>';
+			}
 
-		echo '<br>';
-
-		$stm = $dbh->prepare($sql);
-
-		if($stm->execute([$title, $author, $published, date("Y-m-d H:i:s"), $snippet, $text, $source, $featured])) {
-			echo 'sql query success...<br>';
+			echo $sql;
 		} else {
-			echo 'sql query failed...<br>';
+			echo 'skipping upload<br>';
 		}
-
-		echo $sql;
 	}
+	echo '</ul><br>';
 }
 
-
-
-echo '</ul>';
+autoSubmit('Presidential Signature: ', 'https://www.congress.gov/rss/presented-to-president.xml');
+autoSubmit('House Floor: ', 'https://www.congress.gov/rss/house-floor-today.xml');
+autoSubmit('Senate Floor: ', 'https://www.congress.gov/rss/senate-floor-today.xml');
+autoSubmit('Popular Bill: ', 'https://www.congress.gov/rss/most-viewed-bills.xml');
 ?>
