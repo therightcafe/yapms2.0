ef4fea7b5392213428d1ffe5ccd3a8c67c546b78
Jacob Enders <jacobenders1@gmail.com>
2019-06-28 16:18:26 -0400

    updated master

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,4 @@
-var currentCache = 'v0.24.5';
+var currentCache = 'v0.25.0';
 
 var cookies = {};
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var currentCache = 'v0.24.6';
+var currentCache = 'v0.25.0';
 
 function swLog(message) {
 	console.log('SW' + currentCache + ': ' + message);
