c121dcb9e17f995f00281786a8dc54ed4f344d12
Jacob Enders <jacobenders1@gmail.com>
2019-07-07 02:23:10 -0400

    updated style of map

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var dynamicCache = 'd0.28.0';
-var staticCache = 's0.2.0';
+var dynamicCache = 'd0.28.1';
+var staticCache = 's0.2.1';
 
 function swLog(cache, message) {
 	console.log('SW ' + cache + ': ' + message + ' ( ' + dynamicCache + ' / ' + staticCache + ' )');
