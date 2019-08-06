534f6e87d47a2061d3c9eac651200add02b2e8bb
Jacob Enders <jacobenders1@gmail.com>
2019-08-06 13:38:36 -0400

    min height of 1px to avoid svg matrix error

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,4 @@
-var currentCache = 'v0.42.1';
+var currentCache = 'v0.42.2';
 
 var cookies = {};
 
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var dynamicCache = 'd0.42.1';
-var staticCache = 's0.12.1';
+var dynamicCache = 'd0.42.2';
+var staticCache = 's0.12.2';
 
 function swLog(cache, message) {
 	console.log('SW ' + cache + ': ' + message + ' ( ' + dynamicCache + ' / ' + staticCache + ' )');
