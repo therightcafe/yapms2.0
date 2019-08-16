724c4dcefdd0eafb2adca0bced63c9d27f6f5a55
Jacob Enders <jacobenders1@gmail.com>
2019-08-16 08:32:16 -0400

    update style

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,4 @@
-var currentCache = 'v0.51.4';
+var currentCache = 'v0.51.18';
 
 var cookies = {};
 
@@ -1210,11 +1210,8 @@ function start() {
 
 	var link = document.createElement('link');
 	link.rel = 'stylesheet';
-	//link.href = 'https://use.fontawesome.com/releases/v5.7.2/css/all.css';
 	link.href = './res/fontawesome/css/all.min.css';
 	link.type = 'text/css';
-	//link.integrity = 'sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr';
-	//link.crossorigin = 'anonymous';
 	var ogLink = document.getElementsByTagName('link')[0];
 	ogLink.parentNode.insertBefore(link, ogLink);
 }
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var scriptCache = 'd0.51.17';
-var indexCache = 'i0.51.17';
+var scriptCache = 'd0.51.18';
+var indexCache = 'i0.51.18';
 var staticCache = 's0.51.0';
 
 var _scriptCache = [
