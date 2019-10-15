589cea49c498efbc31e824b50bd5db16cc7d64b7
Jacob Enders <jacobenders1@gmail.com>
2019-10-15 13:41:28 -0400

    update auth

--- login/auth.php
+++ login/auth.php
@@ -1,11 +1,16 @@
 <?
-	require_once "config.php";
+	require_once "GoogleAPI/vendor/autoload.php";
+
 	if(isset($_GET['token'])) {
-		$gClient->getAccessToken($_GET['token']);
-		$oAuth = new Google_Service_Oauth2($gClient);
-		$userData = $oAuth->userinfo_v2_me->get();
-		var_dump($userData);
+		$token = $_GET['token'];
+		$client = new Google_Client(['client_id' => '406738305883-b9cbn6ge3i5a5fnn6perdbuvq1eu5go2.apps.googleusercontent.com']);
+		$payload = $client->verifyIdToken($token);
+		if($payload) {
+			var_dump($payload);
+		} else {
+			echo 'verify error';
+		}
 	} else {
-		echo 'errorrrr';
+		echo 'token not set';
 	}
 ?>
