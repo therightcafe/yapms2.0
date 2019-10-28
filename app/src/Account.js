262ab15aada1bb5879193386389b57ec5e88c7f4
Jacob Enders <jacobenders1@gmail.com>
2019-10-28 16:18:59 -0400

    test

--- app/src/Account.js
+++ app/src/Account.js
@@ -335,9 +335,11 @@ class Account {
 					var name = fileName[3].split('-')[1].split('.')[0];
 					var e = document.createElement('a');
 					e.className = 'selectmenu-button';
-					e.onclick = function() {
-						window.location.href = "https://testing.yapms.com/app/?u=" + Account.id + '$m=' + name;
-					}
+					e.onclick = (function() {
+						return function() {
+							window.location.href = "https://testing.yapms.com/app/?u=" + Account.id + '$m=' + name;
+						}
+					})();
 					var t = document.createElement('div');
 					t.className = "selectmenu-button-text";
 					var text = document.createTextNode(name);
