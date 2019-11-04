74df169e6ad063c09d76c58673e99db38c092a2c
Jacob Enders <jacobenders1@gmail.com>
2019-11-03 22:07:54 -0500

    update text

--- app/html/menu/loginmenu.php
+++ app/html/menu/loginmenu.php
@@ -3,12 +3,12 @@
        <object class="closebutton" type="image/svg+xml">Close</object>
 </div>
 <div class="selectmenu-display-header">
-	<h2 class="selectmenu-display-header-text">Login (beta)</h2>
+	<h2 class="selectmenu-display-header-text">Login - Beta</h2>
 </div>
 </div>
 <div class="selectmenu-content">
-<div class="selectmenu-section" style="word-wrap: break-word;">
-	<small>This login system is currently in beta. If any major issues are found or when the system is fully released, accounts and all saved maps may be deleted.</small>
+<div class="selectmenu-section" style="word-wrap: break-word; max-width: 20em;">
+	<small>This login system is currently in beta. All accounts may be subject to deletion during this phase.</small>
 </div>
 <div class="selectmenu-section" id="login-info">
 	Please enter your credentials
