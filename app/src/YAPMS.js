985c1c273bb5af84d1f193392111a9a03760c8ea
Jacob Enders <jacobenders1@gmail.com>
2019-11-13 00:45:21 -0500

    test

--- app/src/YAPMS.js
+++ app/src/YAPMS.js
@@ -1836,7 +1836,7 @@ class MapLoader {
 			url: URL,
 			type: "POST",
 			success: function(data) {
-				ChartManager.chart.generateLegend();
+			//	ChartManager.chart.generateLegend();
 				console.log("Map Load: Found saved map");
 				try {
 					console.log('Map Loader: Attemping new file load');
