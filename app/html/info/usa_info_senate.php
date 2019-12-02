ba1bca9394ee86aac25c4294e08a731058c072a3
Jacob Enders <jacobenders1@gmail.com>
2019-12-02 14:22:53 -0500

    update

--- app/html/info/usa_info_senate.php
+++ app/html/info/usa_info_senate.php
@@ -1,7 +1,7 @@
 <div id="usa_senate_info" class="sidebar-box sidebar-info-box yesSelect">
 	<h3 class="noSelect">Senate</h3>
 	<p>
-		The United States Senate is the upper legislature of bicameral legislative branch. 
+		The United States Senate is the upper legislature of the bicameral legislative branch. Two senators from each state are elected to represent their state. The goal of having two senators per state is to have each state equally represented in the senate.
 	</p>
 	<p>
 		For more information please visit...
