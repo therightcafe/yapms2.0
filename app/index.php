a9ab2721fae244cca19ed0c9deb1aef68391662b
Jacob Enders <jacobenders1@gmail.com>
2019-12-04 21:01:20 -0500

    fix size

--- app/index.php
+++ app/index.php
@@ -305,9 +305,8 @@ if($mobile === false) {
 			, '</h1>
 		</div>
 
-		<!--style="display:inline-block;min-width:336px;height:280px"-->
 		<ins class="adsbygoogle adslot_sidebar"
-			style="display:inline-block;min-width:250;height:336;"
+			style="display:inline-block;width:250px;height:336px;"
 			data-ad-client="ca-pub-1660456925957249"
 			data-ad-slot="8033943742"></ins>
 		<script>
