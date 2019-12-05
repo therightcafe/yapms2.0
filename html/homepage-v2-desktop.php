3cfa137ee0ab6e7e88b695a7fdb04cab71eb02f5
Jacob Enders <jacobenders1@gmail.com>
2019-12-05 18:43:30 -0500

    update

--- html/homepage-v2-desktop.php
+++ html/homepage-v2-desktop.php
@@ -5,7 +5,6 @@
 </div>
 
 <div id="layout">
-
 	<div id="navigation">
 		<div class="navigation-1">
 			<h3 class="navigation-1-header">
@@ -16,31 +15,32 @@
 			</h3>
 			<div class="navigation-2">
 				<div class="navigation-3">
-					<div class="navigation-4">
-					Presidential
-					</div>
-					<div class="navigation-4">
-					Governors
-					</div>
+					<a class="navigation-4" href="./app/?t=USA_2020_presidential">
+						Presidential
+					</a>
+					<a class="navigation-4" href="./app/?t=USA_2020_governors">
+						Governors
+					</a>
 				</div>
 				<div class="navigation-3">
-					<div class="navigation-4">
-					Senate
-					</div>
-					<div class="navigation-4">
-					House
-					</div>
+					<a class="navigation-4" href="./app/?t=USA_2020_senate">
+						Senate
+					</a>
+					<a class="navigation-4" href="./app/?t=USA_2020_house">
+						House
+					</a>
 				</div>
 				<div class="navigation-3">
-					<div class="navigation-4">
-					Democratic Primary
-					</div>
-					<div class="navigation-4">
-					Republican Primary
-					</div>
+					<a class="navigation-4" href="./app/?t=USA_2020_democratic_primary">
+						Democratic Primary
+					</a>
+					<a class="navigation-4" href="./app/?t=USA_2020_republican_primary">
+						Republican Primary
+					</a>
 				</div>
 			</div>
 		</div>
+
 		<div class="navigation-1">
 			<h3 class="navigation-1-header">
 				<img src="app/res/flags/usa.svg">
@@ -50,22 +50,58 @@
 			</h3>
 			<div class="navigation-2">
 				<div class="navigation-3">
-					<div class="navigation-4">
+					<a class="navigation-4" href="./app/?t=USA_2020_cook">
 						Cook Political Report
-					</div>
-					<div class="navigation-4">
-					Inside Elections
-					</div>
+					</a>
+					<a class="navigation-4" href="./app/?t=USA_2020_inside">
+						Inside Elections
+					</a>
 				</div>
 				<div class="navigation-3">
-					<div class="navigation-4">
-					Sabatos Crystal Ball
-					</div>
+					<a class="navigation-4" href="./app/?t=USA_2020_sabatos">
+						Sabatos Crystal Ball
+					</a>
 				</div>
 			</div>
 		</div>
+	
 		<div class="navigation-1">
-			<h3 class="navigation-1-header">
+			<h3>
+				<img src="app/res/flags/usa.svg">
+				<div class="text">
+					House Forecasts
+				</div>
+			</h3>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=USA_2020_house_cook">
+						Cook Political Report
+					</a>
+				</div>
+			</div>
+		</div>
+	
+		<div class="navigation-1">
+			<h3>
+				<img src="app/res/flags/usa.svg">
+				<div class="text">
+					Current Congressional
+				</div>
+			</h3>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=USA_current_senate">
+						Senate
+					</a>
+					<a class="navigation-4" href="./app/?t=USA_current_house">
+						House
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="navigation-1">
+			<h3>
 				<img src="app/res/flags/usa.svg">
 				<div class="text">
 					Upper State Legislatures
@@ -73,43 +109,72 @@
 			</h3>
 			<div class="navigation-2">
 				<div class="navigation-3">
-					<div class="navigation-4">
-					Arkansas
-					</div>
-					<div class="navigation-4">
-					New Jersey
-					</div>
+					<a class="navigation-4" href="./app/?t=Arkansas_2020_state_upper">
+						Arkansas
+					</a>
+					<a class="navigation-4" href="./app/?t=NewJersey_2020_state_upper">
+						New Jersey
+					</a>
 				</div>
 				<div class="navigation-3">
-					<div class="navigation-4">
-						Pennsylvania	
-					</div>
-					<div class="navigation-4">
+					<a class="navigation-4" href="./app/?t=Pennsylvania_2020_state_upper">
+						Pennsylvania
+					</a>
+					<a class="navigation-4" href="./app/?t=Texas_2020_state_upper">
 						Texas
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="navigation-1">
+			<h3 class="navigation-1-header">
+				<img src="app/res/flags/arg.svg">
+				<div class="text">
+				Argentina
+				</div>
+			</h3>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<div class="navigation-4">
+						Chamber of Deputies	
 					</div>
 				</div>
 			</div>
 		</div>
 		<div class="navigation-1">
 			<h3 class="navigation-1-header">
-				<div class="text">
-				Argentina
-				</div>
-			</h3>
-		</div>
-		<div class="navigation-1">
-			<h3 class="navigation-1-header">
+				<img src="app/res/flags/aus.svg">
 				<div class="text">
 				Australia
 				</div>
 			</h3>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<div class="navigation-4">
+						House of Representatives
+					</div>
+				</div>
+			</div>
 		</div>
 		<div class="navigation-1">
 			<h3 class="navigation-1-header">
+				<img src="app/res/flags/bra.svg">
 				<div class="text">
 				Brazil
 				</div>
 			</h3>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<div class="navigation-4">
+						Federal Senate
+					</div>
+				</div>
+				<div class="navigation-3">
+					<div class="navigation-4">
+						Chamber of Deputies
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 	
@@ -135,8 +200,14 @@
 				<h3>
 					Arkansas HOR District 22
 				</h3>
+				<div class="upcoming-info">
+					December 10, 2019
+				</div>
+				<div class="upcoming-info">
+					Primary Election
+				</div>
 				<a href="./app/?t=Arkansas_2020_state_upper">
-					<div class="map-button">
+					<div class="upcoming-info map-button">
 						Arkansas
 					</div>
 				</a>
@@ -145,8 +216,14 @@
 				<h3>
 					Tennessee HOR District 77	
 				</h3>
+				<div class="upcoming-info">
+					December 10, 2019	
+				</div>
+				<div class="upcoming-info">
+					Special Election
+				</div>
 				<a href="./app/?t=Arkansas_2020_state_upper">
-					<div class="map-button">
+					<div class="upcoming-info map-button">
 						Tennessee
 					</div>
 				</a>
