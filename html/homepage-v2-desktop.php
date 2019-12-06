ae2b7306d946329ad6d3307a3c08b39038736dab
Jacob Enders <jacobenders1@gmail.com>
2019-12-05 22:46:43 -0500

    update

--- html/homepage-v2-desktop.php
+++ html/homepage-v2-desktop.php
@@ -7,12 +7,12 @@
 <div id="layout">
 	<div id="navigation">
 		<div class="navigation-1">
-			<h3 class="navigation-1-header">
+			<div class="header">
 				<img src="app/res/flags/usa.svg">
-				<div class="text">
+				<h3 class="text">
 					National Elections
-				</div>
-			</h3>
+				</h3>
+			</div>
 			<div class="navigation-2">
 				<div class="navigation-3">
 					<a class="navigation-4" href="./app/?t=USA_2020_presidential">
@@ -42,52 +42,68 @@
 		</div>
 
 		<div class="navigation-1">
-			<h3 class="navigation-1-header">
+			<div class="header">
 				<img src="app/res/flags/usa.svg">
-				<div class="text">
+				<h3>
 					Presidential Forecasts
-				</div>
-			</h3>
+				</h3>
+			</div>
 			<div class="navigation-2">
 				<div class="navigation-3">
 					<a class="navigation-4" href="./app/?t=USA_2020_cook">
 						Cook Political Report
+						<br>
+						<small>
+							10/29/19
+						</small>
 					</a>
 					<a class="navigation-4" href="./app/?t=USA_2020_inside">
 						Inside Elections
+						<br>
+						<small>
+							9/1/19
+						</small>
 					</a>
 				</div>
 				<div class="navigation-3">
 					<a class="navigation-4" href="./app/?t=USA_2020_sabatos">
 						Sabatos Crystal Ball
+						<br>
+						<small>
+							11/7/19
+						</small>
 					</a>
 				</div>
 			</div>
 		</div>
 	
 		<div class="navigation-1">
-			<h3>
+			<div class="header">
 				<img src="app/res/flags/usa.svg">
-				<div class="text">
+				<h3>
 					House Forecasts
-				</div>
-			</h3>
+				</h3>
+			</div>
 			<div class="navigation-2">
 				<div class="navigation-3">
 					<a class="navigation-4" href="./app/?t=USA_2020_house_cook">
 						Cook Political Report
+						<br>
+						<small>
+							12/2/19
+						</small>
 					</a>
 				</div>
 			</div>
 		</div>
 	
 		<div class="navigation-1">
-			<h3>
+			<div class="header">
 				<img src="app/res/flags/usa.svg">
-				<div class="text">
-					Current Congressional
-				</div>
-			</h3>
+				<h3>
+					Current Congress 
+				</h3>
+			</div>
 			<div class="navigation-2">
 				<div class="navigation-3">
 					<a class="navigation-4" href="./app/?t=USA_current_senate">
@@ -101,12 +117,12 @@
 		</div>
 
 		<div class="navigation-1">
-			<h3>
+			<div class="header">
 				<img src="app/res/flags/usa.svg">
-				<div class="text">
+				<h3>
 					Upper State Legislatures
-				</div>
-			</h3>
+				</h3>
+			</div>
 			<div class="navigation-2">
 				<div class="navigation-3">
 					<a class="navigation-4" href="./app/?t=Arkansas_2020_state_upper">
@@ -126,60 +142,520 @@
 				</div>
 			</div>
 		</div>
+
 		<div class="navigation-1">
-			<h3 class="navigation-1-header">
+			<div class="header">
+				<img src="app/res/flags/usa.svg">
+				<h3>
+					Lower State Legislatures
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Alabama_2020_state_lower">
+						Alabama
+					</a>
+					<a class="navigation-4" href="./app/?t=Alaska_2020_state_lower">
+						Alaska
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Arkansas_2020_state_lower">
+						Arkansas
+					</a>
+					<a class="navigation-4" href="./app/?t=California_2020_state_lower">
+						California
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Colorado_2020_state_lower">
+						Colorado
+					</a>
+					<a class="navigation-4" href="./app/?t=Connecticut_2020_state_lower">
+						Connecticut
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Florida_2020_state_lower">
+						Florida
+					</a>
+					<a class="navigation-4" href="./app/?t=Hawaii_2020_state_lower">
+						Hawaii
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Idaho_2020_state_lower">
+						Idaho
+					</a>
+					<a class="navigation-4" href="./app/?t=Iowa_2020_state_lower">
+						Iowa
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Michigan_2020_state_lower">
+						Michigan
+					</a>
+					<a class="navigation-4" href="./app/?t=Minnesota_2020_state_lower">
+						Minnesota
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=NewJersey_2020_state_lower">
+						New Jersey
+					</a>
+					<a class="navigation-4" href="./app/?t=NewMexico_2020_state_lower">
+						New Mexico
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=NorthDakota_2020_state_lower">
+						North Dakota
+					</a>
+					<a class="navigation-4" href="./app/?t=Oregon_2020_state_lower">
+						Oregon
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Pennsylvania_2020_state_lower">
+						Pennsylvania
+					</a>
+					<a class="navigation-4" href="./app/?t=Texas_2020_state_lower">
+						Texas
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Washington_2020_state_lower">
+						Washington
+					</a>
+					<a class="navigation-4" href="./app/?t=Wisconsin_2020_state_lower">
+						Wisconsin
+					</a>
+				</div>
+			</div>
+		</div>	
+
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/usa.svg">
+				<h3>
+					Other	
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=USA_takeall">
+						Take All
+					</a>
+					<a class="navigation-4" href="./app/?t=USA_presidential_territories">
+						Territories
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=USA_2024_projection">
+						2024
+					</a>
+					<a class="navigation-4" href="./app/?t=USA_proportional">
+						Proportional
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=USA_trump_impeachment_support">
+						Impeachment Support
+						<br>
+						<small>
+							11/5/19
+						</small>
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="navigation-1">
+			<div class="header">
 				<img src="app/res/flags/arg.svg">
-				<div class="text">
-				Argentina
-				</div>
-			</h3>
-			<div class="navigation-2">
-				<div class="navigation-3">
-					<div class="navigation-4">
-						Chamber of Deputies	
-					</div>
-				</div>
+				<h3>
+					Argentina
+				</h3>
 			</div>
-		</div>
-		<div class="navigation-1">
-			<h3 class="navigation-1-header">
-				<img src="app/res/flags/aus.svg">
-				<div class="text">
-				Australia
-				</div>
-			</h3>
 			<div class="navigation-2">
 				<div class="navigation-3">
-					<div class="navigation-4">
-						House of Representatives
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="navigation-1">
-			<h3 class="navigation-1-header">
-				<img src="app/res/flags/bra.svg">
-				<div class="text">
-				Brazil
-				</div>
-			</h3>
-			<div class="navigation-2">
-				<div class="navigation-3">
-					<div class="navigation-4">
-						Federal Senate
-					</div>
-				</div>
-				<div class="navigation-3">
-					<div class="navigation-4">
+					<a class="navigation-4" href="./app/?t=Argentina_chamber_of_deputies">
 						Chamber of Deputies
-					</div>
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/aus.svg">
+				<h3>
+					Australia
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Australia_states">
+						States
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Australia_house_of_representatives">
+						House of Representatives
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/bra.svg">
+				<h3>
+					Brazil
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Brazil_federal_senate">
+							Federal Senate
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Brazil_chamber_of_deputies">
+							Chamber of Deputies
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/can.svg">
+				<h3>
+					Canada
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Canada_house_of_commons">
+						House of Commons
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Canada_2019_house_of_commons">
+						2019 Results
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Canada_provinces">
+						Provinces
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/fra.svg">
+				<h3>
+					France
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=France_national_assembly">
+						National Assembly
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/ger.svg">
+				<h3>
+					Germany
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Germany_bundestag">
+						Bundestag
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Germany_states">
+						States
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/ind.svg">
+				<h3>
+					India
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=India_lok_sabha">
+						Lok Sabha
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=India_2019_lok_sabha">
+						2019 Results
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/ita.svg">
+				<h3>
+					Italy
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Italy_states">
+						States
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/ire.svg">
+				<h3>
+					Ireland
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Ireland_dail_eireann">
+						Dáil Éireann
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/ned.svg">
+				<h3>
+					Netherlands
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Netherlands_gemeenten">
+						Gemeeten
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Netherlands_provinces">
+						Provinces
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/prt.svg">
+				<h3>
+					Portugal
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Portugal_assembly_of_the_republic">
+						Assembly of the Republic
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/rus.svg">
+				<h3>
+					Russia
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Russia_federal_council">
+						Federation Council
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Russia_duma">
+						Duma
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/zaf.svg">
+				<h3>
+					South Africa
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=SouthAfrica_national_assembly">
+						National Assembly
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/esp.svg">
+				<h3>
+					Spain
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Spain_congress_of_deputies">
+						Congress of Deputies
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/swe.svg">
+				<h3>
+					Sweden
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Sweden_riksdag">
+						Riksdag
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/che.svg">
+				<h3>
+					Switzerland
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Switzerland_council_of_states">
+						Council of States
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Switzerland_national_council">
+						National Council
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/tur.svg">
+				<h3>
+					Turkey
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Turkey_national_assembly">
+						National Assembly
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/tat.svg">
+				<h3>
+					Trinidad and Tobago
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=Trinidad_Tobago_house_of_representatives">
+						House of Representatives
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="navigation-1">
+			<div class="header">
+				<img src="app/res/flags/ukd.svg">
+				<h3>
+					United Kingdom
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=UnitedKingdom_house_of_commons">
+						House of Commons
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=UnitedKingdom_current_parliament">
+						Current Parliament
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=UnitedKingdom_historic_counties">
+						Historic Counties
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="navigation-1">
+			<div class="header">
+				<h3>
+					Global
+				</h3>
+			</div>
+			<div class="navigation-2">
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=World">
+						United Nations
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=EuropeanUnion">
+						European Union
+					</a>
+				</div>
+				<div class="navigation-3">
+					<a class="navigation-4" href="./app/?t=USA_Canada">
+						USA / Canada
+					</a>
 				</div>
 			</div>
 		</div>
 	</div>
 	
 	<div id="featured">
-		
 		<div id="infobox">
 			<p>
 			YAPms is a tool for creating and sharing political maps.
@@ -192,40 +668,114 @@
 
 		<br>
 
-		<div id="upcoming">
+		<div id="New Maps" class="featured-section">
+			<h2>
+				New Maps
+			</h2>
+			<div class="info-box">
+				<h3>
+					Cook Political Report	
+				</h3>
+				<a class="map-button" href="./app/?t=USA_2020_house_cook">
+					House
+					<br>
+					<small>
+						December 2, 2019
+					</small>
+				</a>
+			</div>
+		</div>
+
+		<div id="popular-maps" class="featured-section">
+			<h2>
+				Popular Maps
+			</h2>
+			<div class="info-box">
+				<h3>
+					United States
+				</h3>
+				<a class="map-button" href="./app/?=USA_2020_presidential">
+					Presidential
+				</a>
+				<a class="map-button" href="./app/?t=USA_2020_senate">
+					Senate
+				</a>
+				<a class="map-button" href="./app/?t=USA_2020_house">
+					House
+				</a>
+			</div>
+			<div class="info-box">
+				<h3>
+					United Kingdom
+				</h3>
+				<a class="map-button" href="./app/?t=UnitedKingdom_house_of_commons">
+					House of Commons
+				</a>
+				<a class="map-button" href="./app/?t=UnitedKingdom_current_parliament">
+					Current Parliament
+				</a>
+			</div>
+		</div>
+
+		<div id="upcoming" class="featured-section">
 			<h2>
 				Upcoming Elections
 			</h2>
-			<div class="upcoming-election">
+			<div class="info-box">
 				<h3>
 					Arkansas HOR District 22
 				</h3>
-				<div class="upcoming-info">
-					December 10, 2019
-				</div>
-				<div class="upcoming-info">
+				<div class="text">
 					Primary Election
+					<br>
+					<small>
+						December 10, 2019
+					</small>
 				</div>
-				<a href="./app/?t=Arkansas_2020_state_upper">
-					<div class="upcoming-info map-button">
-						Arkansas
-					</div>
+				<a class="map-button" href="./app/?t=Arkansas_2020_state_lower">
+					House
 				</a>
 			</div>
-			<div class="upcoming-election">
+			<div class="info-box">
 				<h3>
 					Tennessee HOR District 77	
 				</h3>
-				<div class="upcoming-info">
-					December 10, 2019	
-				</div>
-				<div class="upcoming-info">
+				<div class="text">
 					Special Election
+					<br>
+					<small>
+						December 10, 2019
+					</small>
 				</div>
-				<a href="./app/?t=Arkansas_2020_state_upper">
-					<div class="upcoming-info map-button">
-						Tennessee
-					</div>
+			</div>
+			<div class="info-box">
+				<h3>
+					United Kingdom
+				</h3>
+				<div class="text">
+					Parliamentary
+					<br>
+					<small>
+						December 12, 2019
+					</small>
+				</div>
+				<a class="map-button" href="./app/?t=UnitedKingdom_house_of_commons">
+					House of Commons
+				</a>
+			</div>
+			<div class="info-box">
+				<h3>
+					Trinidad and Tobago
+				</h3>
+				<div class="text">
+					General
+					<br>
+					<small>
+						November 6, 2020
+					</small>
+				</div>
+				<a class="map-button" href="./app/?t=Trinidad_Tobago_house_of_representatives">
+					House of Representatives
 				</a>
 			</div>
 		</div>
